cfdde94951c312904bbea6ec99104bace2bfb28c
Rodion <klapan7x@gmail.com>
2021-08-30 10:52:06 +0300

    lesson4_hw

--- src/App.js
+++ src/App.js
@@ -3,25 +3,23 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Link, Route, Router} from "react-router-dom";
-
+import { BrowserRouter, Link, Route, Router } from "react-router-dom";
 
 function App(props) {
   return (
-
     <div className="app-wrapper">
       <BrowserRouter>
-        <Header/>
-        <Navbar/>
-        <div className='app-wrapper-content'>
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
           <switch>
-            <Route path='/Dialogs' component={Dialogs}/>
-            <Route path='/Profile' component={Profile}/>
+            <Route path="/Dialogs" component={Dialogs} />
+            <Route path="/Profile" component={Profile} />
           </switch>
         </div>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App;
